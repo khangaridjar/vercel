73f77609a9d90c4f731a4ef587bb6a73fd028bcf
Your Name <you@example.com>
2024-11-22 11:52:44 +0800

    2doh oorchlolt

--- khangarid/pages/weatherapp.js
+++ khangarid/pages/weatherapp.js
@@ -5,9 +5,13 @@ import localFont from "next/font/local";
 
 export default function Home() {
   return (
-    <div className="bg-slate-900 h-screen w-full">
+    <div className="bg-slate-900 h-screen w-full flex flex-row items-end">
         <div className="bg-slate-800 h-screen w-48 rounded rounded-2xl"></div>
-        <div className="bg-slate-800 h-64 w-32 rounded rounded-2xl flex justify-center items-bottom">
+        <div className="flex flex-col">
+        <div className="bg-slate-800 h-64 w-[65%] rounded rounded-2xl flex justify-center items-bottom ">
+
+        </div>
+
         </div>
     </div>
   );
